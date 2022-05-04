77217f3752b3b44495fb337bf755a5b7baa14106
Alexey Poletaev <av.poletaev21@gmail.com>
2022-05-04 17:33:59 +0300

    Initial commit

---
+++ CatBreeds/CatBreedsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatBreedsApp.swift
+//  CatBreeds
+//
+//  Created by Alexey Poletaev on 04.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct CatBreedsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
