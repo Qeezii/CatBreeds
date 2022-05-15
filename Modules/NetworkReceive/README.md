82fac057dfffd100e81ba82c951247b765eb9c07
Alexey Poletaev <av.poletaev21@gmail.com>
2022-05-15 17:07:41 +0300

    Correct

---
+++ Modules/NetworkReceive/README.md
@@ -0,0 +1,3 @@
+# NetworkReceive
+
+A description of this package.
---
+++ Modules/ServiceLocator/Sources/ServiceLocator/ServiceLocator.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+public class ServiceLocator {
+    
+    public static let shared = ServiceLocator()
+    
+    lazy var services: [String: AnyObject] = {
+        return [String:AnyObject]()
+    }()
+    
+    public func addServices<T:AnyObject>(service: T) {
+        let key = String(describing: T.self)
+        if services[key] == nil {
+            services[key] = service
+        }
+    }
+    
+    public func resolve<T:AnyObject>(type: T.Type)->T? {
+        let key = String(describing: type)
+        return services[key] as? T
+    }
+}
